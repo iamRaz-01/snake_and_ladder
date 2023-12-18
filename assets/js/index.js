e81c8b1e679e2fb34490715b81cdf07ccf2f4150
Abdul razak <abdulrazak.nasriudeen@gmail.com>
2023-12-18 11:30:40 +0530

    created functions to index page

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+let head =  document.getElementById("headDiv");
+let player =  document.getElementById("players");
+let startBtn =  document.getElementById("startBtn");
+startBtn.addEventListener("click",(e)=>{
+    head.style.display ="none"
+    player.style.display ="flex"
+   
+})
+
